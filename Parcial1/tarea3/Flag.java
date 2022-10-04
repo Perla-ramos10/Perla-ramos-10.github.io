f9fb69f91ca59eba1c682ef039349537742b14de
Perla-ramos10 <ramosperla848@gmail.com>
2022-10-04 18:47:26 -0500

    se agrego range. java

---
+++ Parcial1/tarea3/Flag.java
@@ -0,0 +1,15 @@
+/*
+*Flag.java
+*Author: Perla Ramos
+*Date: Oct 4, 2022
+*Location: Ixtac, Ver, Mexico 
+*/
+class Flag { // name of programa 
+  public static void main(String[] args) { // main method 
+    	
+   boolean flag = true;         //define a bolean var
+   
+   System.out.println (!flag);  //prints true 
+
+  }
+}
